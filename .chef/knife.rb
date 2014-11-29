81bbc54735c927cbebd36fc0692a59d24776c10e
Drew Blessing <drew.blessing@me.com>
2014-11-29 11:48:27 -0600

    add validation

--- .chef/knife.rb
+++ .chef/knife.rb
@@ -1,6 +1,8 @@
 user = ENV['OPSCODE_USER'] || ENV['USER']
 node_name                   user
 client_key                  "#{ENV['HOME']}/.chef/#{user}.pem"
+validation_client_name      'bke-validator'
+validation_key              "#{ENV['HOME']}/.chef/bke-validation.pem"
 chef_server_url             "https://#{ENV['CHEF_SERVER_URL']}/organizations/bke"
 syntax_check_cache_path     "#{ENV['HOME']}/.chef/syntax_check_cache"
 cookbook_path               ["#{ENV['CHEF_WORKSPACE']}/cookbooks"]
